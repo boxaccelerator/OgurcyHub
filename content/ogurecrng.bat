d8b9125b8f72cd703081eb58b0046f21a5af629d
Yozha <130782610+RealYozha@users.noreply.github.com>
2024-12-01 21:15:28 +0300

    v3.3.4+70ab35d8-03e0-4092-9764-36371925b7de

--- content/ogurecrng.bat
+++ content/ogurecrng.bat
@@ -4,9 +4,9 @@ mode 100, 35
 goto Main
 
 :Main
-set /a minRn = 1
-set /a maxRn = 20000
-set /a random=((( %RANDOM% %% (%maxRn% - %minRn% + 1) ) + %minRn%) * 50) - 49
+set /a minRnd = 1
+set /a maxRnd = 20000
+set /a rnd=((( %RANDOM% %% (%maxRnd% - %minRnd% + 1) ) + %minRnd%) * 50) - 49
 set /a cheId=%RANDOM% + %RANDOM% + %RANDOM%
 @REM EQU - equal
 @REM NEQ - not equal
@@ -15,56 +15,56 @@ set /a cheId=%RANDOM% + %RANDOM% + %RANDOM%
 @REM GTR - greater than
 @REM GEQ - greater than or equal
 color f
-echo [Common] Ogurec > .\cache\%cheId%.ogurecche
+echo [Common] Ogurec
 color 2
-if %random% LEQ 650000 (
+if %rnd% LEQ 650000 (
     color f
-    echo [Common] Fresh ogurec > .\cache\%cheId%.ogurecche
+    echo [Common] Fresh ogurec
     color a
 )
-if %random% LEQ 350000 (
+if %rnd% LEQ 350000 (
     color f
-    echo [Uncommon] Coleny ogurec > .\cache\%cheId%.ogurecche
+    echo [Uncommon] Coleny ogurec
     color 2
 )
-if %random% LEQ 150000 (
+if %rnd% LEQ 150000 (
     color f
-    echo [Uncommon] Supermarinovanny ogurec > .\cache\%cheId%.ogurecche
+    echo [Uncommon] Supermarinovanny ogurec
     color b
 )
-if %random% LEQ 70000 (
+if %rnd% LEQ 70000 (
     color f
-    echo [Rare] Nautilus ogurec > .\cache\%cheId%.ogurecche
+    echo [Rare] Nautilus ogurec
     color 9
 )
-if %random% LEQ 50000 (
+if %rnd% LEQ 50000 (
     color f
-    echo [Rare] Supermarinovanny x3 ogurec > .\cache\%cheId%.ogurecche
+    echo [Rare] Supermarinovanny x3 ogurec
     color 3
 )
-if %random% LEQ 35000 (
+if %rnd% LEQ 35000 (
     color f
-    echo [Rare] Nautilus x2 ogurec > .\cache\%cheId%.ogurecche
+    echo [Rare] Nautilus x2 ogurec
     color 9
 )
-if %random% LEQ 1000 (
+if %rnd% LEQ 1000 (
     color f
-    echo [LEGENDARY] Grandma's ogurec > .\cache\%cheId%.ogurecche
+    echo [LEGENDARY] Grandma's ogurec
     color 2
 )
-if %random% LEQ 100 (
+if %rnd% LEQ 100 (
     color f
-    echo [MYTHIC] Cool ogurec > .\cache\%cheId%.ogurecche
+    echo [MYTHIC] Cool ogurec
     color 0
 )
-if %random% LEQ 10 (
+if %rnd% LEQ 10 (
     color f
-    echo [EXALTED] Sigma ogurec > .\cache\%cheId%.ogurecche
+    echo [EXALTED] Sigma ogurec
     color 7
 )
-if %random% LEQ 1 (
+if %rnd% LEQ 1 (
     color f
-    echo [GLORIOUS] HOLY OGUREC > .\cache\%cheId%.ogurecche
+    echo [GLORIOUS] HOLY OGUREC
     color e
 )
 echo    ▒▒▓▓▓▓    ░░░░░░
@@ -86,7 +86,8 @@ echo  ▒▒██▓▓▓▓██
 echo    ██▓▓▓▓██        
 echo    ▓▓▓▓▓▓▓▓        
 echo    ██▓▓██          
-echo [ Roll: %random% ] > .\cache\%cheId%.ogurecche
-echo [ Rolling ] > .\cache\%cheId%.ogurecche
+echo [ Roll: %rnd% ]
+echo [ Rolling ]
+echo %rnd% > .\cache\%cheId%.ogurecrngche
 timeout 1
 ogurecrng
